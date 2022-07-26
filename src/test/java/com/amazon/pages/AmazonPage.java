505d8adb6d68471c73c7dc80242418442ae68305
hrnoksz <harun1973@yahoo.com>
2022-07-26 16:03:01 +0300

    updated

--- src/test/java/com/amazon/pages/AmazonPage.java
+++ src/test/java/com/amazon/pages/AmazonPage.java
@@ -1,12 +1,27 @@
 package com.amazon.pages;
 
 import com.amazon.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class AmazonPage {
 
     public AmazonPage(){
 
         PageFactory.initElements(Driver.getDriver(), this);
+
     }
+    @FindBy(id = "a-autoid-0")
+    public WebElement cookies;
+
+    @FindBy(id = "twotabsearchtextbox")
+    public WebElement searchBtn;
+
+    @FindBy(xpath = "//span[@class='a-size-base-plus a-color-base a-text-normal']")
+    public List<WebElement> phoneList;
+
+
 }
--- src/test/java/com/amazon/stepDefinitions/amazon_stepDefinition.java
+++ src/test/java/com/amazon/stepDefinitions/amazon_stepDefinition.java
@@ -1,23 +1,52 @@
 package com.amazon.stepDefinitions;
 
+import com.amazon.pages.AmazonPage;
 import com.amazon.utilities.ConfigurationReader;
 import com.amazon.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class amazon_stepDefinition {
+
+    AmazonPage page = new AmazonPage();
 
     @Given("buyer at Amazon home page")
     public void buyer_at_amazon_home_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        if(page.cookies.isDisplayed()){
+            page.cookies.click();
+        }
     }
-    @When("buyer searches {string}")
-    public void buyer_searches(String string) {
 
+    @When("buyer searches {string}")
+    public void buyer_searches(String productName) {
+        page.searchBtn.sendKeys(ConfigurationReader.getProperty("productName")+ Keys.ENTER);
     }
     @Then("the results are listed")
     public void the_results_are_listed() {
+
+        List<WebElement> result = page.phoneList;
+        List<String> phoneListName = new LinkedList<>();
+
+        for (WebElement each : result) {
+            if (each.getText().contains("iPhone 13 (512 GB)") || each.getText().contains("iPhone 13 mini")
+                    || each.getText().contains("Apple iPhone 13 Pro")) {
+                phoneListName.add(each.getText());
+            }
+
+        }
+        System.out.println("phoneListName = " + phoneListName);
+
+        System.out.println(phoneListName.size());
+
+        Assert.assertEquals(8, phoneListName.size() );
 
     }
     @Then("buyer click on {string}")
